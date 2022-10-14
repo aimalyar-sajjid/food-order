c716a7bcb75775ee4ec0060e6147c228e70b513b
Aimalyar Sajjid <aimalyar2018@gmail.com>
2022-10-14 22:52:48 +0430

    Third Push

--- common/navbar.php
+++ common/navbar.php
@@ -23,6 +23,50 @@
       <!-- SEARCH BUTTON -->
         <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
 
+        <div class="search-result-body">
+          <div class="container px-5 mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div>
+                      <div class="search-input">
+                        <input type="text" name="search" id="search" class="form-control" placeholder="Search...">
+                        <span id="close-search"><i class="fa-solid fa-xmark"></i></span>
+                      </div>
+                      <div class="search-result">
+                        <table class="table text-white">
+                          <tr>
+                            <td><h3>Teasty Pizza</h3></td>
+                            <td><img src="assets/images/dish-2.png" width="70px" alt=""></td>
+                          </tr>
+
+                          <tr>
+                            <td><h3>Teasty Burger</h3></td>
+                            <td><img src="assets/images/dish-3.png" width="70px" alt=""></td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
     </div>
   </div>
 </nav>
+
+<script>
+  const searchBtn = document.querySelector(".search-btn");
+  const closeBtn = document.querySelector("#close-search");
+  const searchBody = document.querySelector(".search-result-body");
+
+  searchBtn.addEventListener("click", ()=>{
+    searchBody.style.opacity = 1;
+    searchBody.style.pointerEvents = "auto";
+  });
+
+  closeBtn.addEventListener("click", ()=>{
+    searchBody.style.opacity = 0;
+    searchBody.style.pointerEvents = "none";
+  });
+</script>
